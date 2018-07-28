a5e54dd2ac794ae8719cd66678d2efac9b9bdaaf
willix-win <benavidesb@unicauca.edu.co>
2018-07-28 12:34:25 -0500

    24:00 add register button and console log username, email and password

--- app/src/main/java/com/willix/kotlinmessenger/MainActivity.kt
+++ app/src/main/java/com/willix/kotlinmessenger/MainActivity.kt
@@ -2,11 +2,22 @@ package com.willix.kotlinmessenger
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        register_button_register.setOnClickListener {
+            val email = email_edittext_register.text.toString()
+            val password = password_edittext_register.text.toString()
+
+            Log.d("MainActivity", "Email is: " + email)
+            Log.d("MainActivity", "Password: $password")
+        }
+
     }
 }
